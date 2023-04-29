81c023cc291c6bf8bec4f5982da9c40108cfd87f
Berkan Aslan <aslnberkan@gmail.com>
2023-04-29 18:49:42 +0300

    SpringBoot v3.0.6 initialized.

---
+++ src/test/java/com/berkanaslan/springboot3firstblush/SpringBoot3FirstBlushApplicationTests.java
@@ -0,0 +1,13 @@
+package com.berkanaslan.springboot3firstblush;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoot3FirstBlushApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
